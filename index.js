87759a3d52faad516b4c22141368da2a757481d2
Sander Sierra <sierrasander29@gmail.com>
2023-01-27 10:23:42 -0500

    feat: Mount routes

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ import express from "express"
 import cors from "cors"
 import morgan from "morgan"
 
+import mountRoutes from "./src/routes/index.js"
+
 const host = config.get("server.host")
 const port = config.get("server.port")
 
 const app = express()
-
 app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
+
+mountRoutes(app)
 
 const listener = app.listen(port, host, () => {
     console.log(`Server listening on port ${listener.address().port}`)
